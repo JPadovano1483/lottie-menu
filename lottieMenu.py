eb7f26f2a998be088b7d036b4e1b1a840860b5b8
Jamie Padovano <jp1483@messiah.edu>
2022-07-29 18:00:06 -0400

    added logic and user input

--- lottieMenu.py
+++ lottieMenu.py
@@ -1,64 +1,61 @@
-import requests
 from requests import get
-from bs4 import BeautifulSoup
 import time
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import undetected_chromedriver as uc
+
+
 
 def get_menu():
   return driver.find_elements(By.XPATH, "//td[@class='cbo_nn_itemHover']")
 
 if __name__ == '__main__':
+  print('Meal: ')
+  meal = input()
+
   # open link
   driver = uc.Chrome()
   driver.get("https://www.messiah.edu/NetNutrition/1")
 
   # click lottie button
   driver.find_element(By.XPATH, "//td[@class='cbo_nn_unitsCell']/a").click()
-  time.sleep(1)
+  time.sleep(0.1)
 
   # click main menu button
   driver.find_element(By.XPATH, "//div[@class='cbo_nn_unitImages']/a").click()
-  time.sleep(1)
+  time.sleep(0.1)
 
-  # click breakfast button
-  driver.find_element(By.XPATH, "//tr[@class='cbo_nn_menuPrimaryRow']/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td[1]/a").click()
-  time.sleep(1)
+  if (meal == 'breakfast'):
+    # click breakfast button
+    driver.find_element(By.XPATH, "//tr[@class='cbo_nn_menuPrimaryRow']/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td[1]/a").click()
+    time.sleep(0.2)
 
-  # get breakfast menu
-  breakfastMenu = get_menu()
-  breakfastMenu = [item.text for item  in breakfastMenu]
-  time.sleep(1)
+    # get breakfast menu
+    breakfastMenu = get_menu()
+    breakfastMenu = [item.text for item in breakfastMenu]
+    print(breakfastMenu)
 
-  # click back button
-  driver.find_element(By.ID, 'btn_Back1').click()
-  time.sleep(1)
+  elif (meal == 'lunch'):
+    # click lunch button
+    driver.find_element(By.XPATH, "//tr[@class='cbo_nn_menuPrimaryRow']/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td[2]/a").click()
+    time.sleep(0.2)
 
-  # click lunch button
-  driver.find_element(By.XPATH, "//tr[@class='cbo_nn_menuPrimaryRow']/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td[2]/a").click()
-  time.sleep(1)
+    # get lunch menu
+    lunchMenu = get_menu()
+    lunchMenu = [item.text for item in lunchMenu]
+    print(lunchMenu)
 
-  # get lunch menu
-  lunchMenu = get_menu()
-  lunchMenu = [item.text for item in lunchMenu]
-  time.sleep(1)
+  elif (meal == 'dinner'):
+    # click dinner button
+    driver.find_element(By.XPATH, "//tr[@class='cbo_nn_menuPrimaryRow']/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td[3]/a").click()
+    time.sleep(0.2)
 
-  # click back button
-  driver.find_element(By.ID, 'btn_Back1').click()
-  time.sleep(1)
+    # get dinner menu
+    dinnerMenu = get_menu()
+    dinnerMenu = [item.text for item in dinnerMenu]
+    print(dinnerMenu)
 
-  # click dinner button
-  driver.find_element(By.XPATH, "//tr[@class='cbo_nn_menuPrimaryRow']/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td[3]/a").click()
-  time.sleep(1)
+  else:
+    print("That's not a meal you fucking idiot!")
 
-  # get dinner menu
-  dinnerMenu = get_menu()
-  dinnerMenu = [item.text for item in dinnerMenu]
-  time.sleep(1)
-
-  print(breakfastMenu)
-  print(lunchMenu)
-  print(dinnerMenu)
 
 
